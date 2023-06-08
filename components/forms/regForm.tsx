2f206709cfeea5de8b76e22993537dfcd759896c
iamfromhe1l <sgadgsfad@gmail.com>
2023-06-08 03:02:38 +0300

    auth lg-design

--- components/forms/regForm.tsx
+++ components/forms/regForm.tsx
@@ -34,7 +34,6 @@ const RegForm = () => {
         )}
       >
         <div>
-          <label htmlFor='name'>Имя</label>
           <input
             {...register('name', {
               required: true,
@@ -44,7 +43,7 @@ const RegForm = () => {
             aria-invalid={errors.name ? 'true' : 'false'}
             type='text'
             id='name'
-            placeholder='Maksim'
+            placeholder='Имя'
             className={errors.name ? s.errorInput : ''}
           />
           {errors.name && errors.name.type == 'required' && (
@@ -58,7 +57,6 @@ const RegForm = () => {
           )}
         </div>
         <div>
-          <label htmlFor='email'>Почта</label>
           <input
             {...register('email', {
               required: true,
@@ -67,7 +65,7 @@ const RegForm = () => {
             aria-invalid={errors.email ? 'true' : 'false'}
             type='text'
             id='email'
-            placeholder='maksimIvanov2004@gmail.com'
+            placeholder='Email'
             className={errors.email ? s.errorInput : ''}
           />
           {errors.email && errors.email.type == 'required' && (
@@ -78,7 +76,6 @@ const RegForm = () => {
           )}
         </div>
         <div>
-          <label htmlFor='password'>Пароль</label>
           <input
             {...register('password', {
               required: true,
@@ -88,7 +85,7 @@ const RegForm = () => {
             aria-invalid={errors.password ? 'true' : 'false'}
             type='password'
             id='password'
-            placeholder='qwertyuiop'
+            placeholder='Пароль'
             className={errors.password ? s.errorInput : ''}
           />
           {errors.password && errors.password.type == 'required' && (
@@ -101,7 +98,9 @@ const RegForm = () => {
             <p className={s.errorMessage}>Password max length is 16</p>
           )}
         </div>
-        <input type='submit' />
+        <div>
+          <input type='submit' className={s.btn} />
+        </div>
       </form>
       <div>{error}</div>
     </>
